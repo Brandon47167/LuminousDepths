4d39b5971f976eb410df57de9632bca29ea8ca53
brandon47167 <superyoshifan101@gmail.com>
2020-07-09 21:51:35 -0700

    Added blocks.

---
+++ src/main/java/com/brandon47167/ld/blocks/SeafoamCoral.java
@@ -0,0 +1,17 @@
+package com.brandon47167.ld.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class SeafoamCoral extends Block {
+    public SeafoamCoral() {
+        super(Properties.create(Material.IRON)
+                .hardnessAndResistance(3.0f, 6.0f)
+                .sound(SoundType.METAL)
+                .harvestLevel(3)
+                .harvestTool(ToolType.PICKAXE)
+        );
+    }
+}
--- src/main/java/com/brandon47167/ld/util/RegistryHandler.java
+++ src/main/java/com/brandon47167/ld/util/RegistryHandler.java
@@ -1,9 +1,7 @@
 package com.brandon47167.ld.util;
 
 
-import com.brandon47167.ld.blocks.BlockItemBase;
-import com.brandon47167.ld.blocks.ChiseledPearlBlock;
-import com.brandon47167.ld.blocks.DeadSeafoamBlock;
+import com.brandon47167.ld.blocks.*;
 import com.brandon47167.ld.items.ItemBase;
 import com.brandon47167.ld.ld;
 import net.minecraft.block.Block;
@@ -27,15 +25,23 @@ public class RegistryHandler {
     public static final RegistryObject<Item> isocarapace = ITEMS.register("isocarapace", ItemBase::new);
     public static final RegistryObject<Item> TungstenIngot = ITEMS.register("tungsten_ingot", ItemBase::new);
     public static final RegistryObject<Item> TungstenNugget = ITEMS.register("tungsten_nugget", ItemBase::new);
+    public static final RegistryObject<Item> rainbow_fish = ITEMS.register("rainbow_fish", ItemBase::new);
     // Blocks
 
     public static final RegistryObject<Block> Chiseled_Pearl_Block = BLOCKS.register("chiseled_pearl_block", ChiseledPearlBlock::new);
 
     public static final RegistryObject<Block> Dead_Seafoam_Block = BLOCKS.register("dead_seafoam_block", DeadSeafoamBlock::new);
 
+    public static final RegistryObject<Block> Tungsten_Block = BLOCKS.register("tungsten_block", TungstenBlock::new);
+
+    public static final RegistryObject<Block> Seafoam_Coral_Block = BLOCKS.register("seafoam_coral", SeafoamCoral::new);
+
     // Block Items
     public static final RegistryObject<Item> Chiseled_Pearl_Item = ITEMS.register("chiseled_pearl_block", () -> new BlockItemBase(Chiseled_Pearl_Block.get()));
 
     public static final RegistryObject<Item> Dead_Seafoam_Item = ITEMS.register("dead_seafoam_block", () -> new BlockItemBase(Dead_Seafoam_Block.get()));
+    public static final RegistryObject<Item> Tungsten_Block_Item = ITEMS.register("tungsten_block", () -> new BlockItemBase(Tungsten_Block.get()));
+
+    public static final RegistryObject<Item> Seafoam_Coral = ITEMS.register("seafoam_coral", () -> new BlockItemBase(Seafoam_Coral_Block.get()));
 
 }
